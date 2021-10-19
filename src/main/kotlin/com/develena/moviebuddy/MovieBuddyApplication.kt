eb1fea10cb4bb9fe4c309e7e5246c169d39afb93
hyunhee <hyunhee@nuritelecom.com>
2021-10-19 22:48:52 +0900

    Initial commit

---
+++ src/main/kotlin/com/develena/moviebuddy/MovieBuddyApplication.kt
@@ -0,0 +1,11 @@
+package com.develena.moviebuddy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieBuddyApplication
+
+fun main(args: Array<String>) {
+    runApplication<MovieBuddyApplication>(*args)
+}
---
+++ src/test/kotlin/com/develena/moviebuddy/MovieBuddyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.develena.moviebuddy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieBuddyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
